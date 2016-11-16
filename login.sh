5a34fe9e83cf8c9dd48e0deb4a48ddcbfaa5691c
adammau2 <adam.mau21@gmail.com>
2016-11-16 19:11:17 +0700

    Add files via upload

---
+++ login.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+echo "-----------------------------------------";
+echo "------------ Dropbear Log In ------------";
+echo "-----------------------------------------";
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+echo "-----------------------------------------";
+
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+echo "------------ OpenSSH login --------------";
+echo "-----------------------------------------";
+for PID in "${data[@]}"
+do
+        #echo "check $PID";
+        NUM=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+        USER=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+        IP=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+done
+
+echo "Untuk kill user log in, gunakan command  ";
+echo "kill -9 (nomerid)                        ";
+echo "contoh : kill -9 28651 ";
+echo "-----------------------------------------";
+echo "------------  Adam Maulana  -------------";
+echo "------------  089639714655  -------------";
+echo "-----------------------------------------";
+
