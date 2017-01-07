dab9eb268dda1cb29d399cf00a1cc6b1754a358f
Adam Maulana <adam.mau21@gmail.com>
2017-01-07 08:57:13 +0700

    Create Account SSH

--- create-user.sh
+++ create-user.sh
@@ -6,7 +6,7 @@ read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
-IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0`
+IP=`curl ifconfig.me/ip`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
