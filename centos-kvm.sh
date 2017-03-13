b84f20044a1bc819110a6333e37ad3047ea70ca9
Khairil Gunawan <khairilgunawan@live.com>
2017-03-13 17:15:15 +0800

    Update centos-kvm.sh

--- centos-kvm.sh
+++ centos-kvm.sh
@@ -15,14 +15,14 @@ hostname "$hnbaru"
 echo "Hostname telah diganti menjadi $hnbaru"
 
 # Banner SSH
-echo "## SELAMAT DATANG DI SERVER PREMIUM $hnbaru ## " > /etc/pesan
-echo "DENGAN MENGGUNAKAN LAYANAN SSH DARI SERVER INI BERARTI ANDA SETUJU SEGALA KETENTUAN YANG TELAH KAMI BUAT: " > /etc/pesan
-echo "1. Tidak diperbolehkan untuk melakukan aktivitas illegal seperti DDoS, Hacking, Phising, Spam, dan Torrent di server ini; " > /etc/pesan
-echo "2. Pengguna setuju jika kami mengetahui atau sistem mendeteksi pelanggaran di akunnya maka akun akan dihapus oleh sistem; " > /etc/pesan
-echo "3. Tidak ada tolerasi bagi pengguna yang melakukan pelanggaran; " > /etc/pesan
-echo "Server by $namap ( $nhp )" >/etc/pesan
+echo "## SELAMAT DATANG DI SERVER PREMIUM $hnbaru ## " >> /etc/pesan
+echo "DENGAN MENGGUNAKAN LAYANAN SSH DARI SERVER INI BERARTI ANDA SETUJU SEGALA KETENTUAN YANG TELAH KAMI BUAT: " >> /etc/pesan
+echo "1. Tidak diperbolehkan untuk melakukan aktivitas illegal seperti DDoS, Hacking, Phising, Spam, dan Torrent di server ini; " >> /etc/pesan
+echo "2. Pengguna setuju jika kami mengetahui atau sistem mendeteksi pelanggaran di akunnya maka akun akan dihapus oleh sistem; " >> /etc/pesan
+echo "3. Tidak ada tolerasi bagi pengguna yang melakukan pelanggaran; " >> /etc/pesan
+echo "Server by $namap ( $nhp )" >> /etc/pesan
 
-echo "Banner /etc/pesan" > /etc/ssh/sshd_config
+echo "Banner /etc/pesan" >> /etc/ssh/sshd_config
 
 # update software server
 yum update -y
