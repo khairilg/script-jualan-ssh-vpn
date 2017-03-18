a7829f0d7795ce3f97a0cf39987e3038a8990fea
Khairil Gunawan <khairilgunawan@live.com>
2017-03-18 17:55:22 +0800

    Update centos-kvm.sh

--- centos-kvm.sh
+++ centos-kvm.sh
@@ -21,8 +21,8 @@ echo "## SELAMAT DATANG DI SERVER PREMIUM $hnbaru ## " >> /etc/pesan
 echo "DENGAN MENGGUNAKAN LAYANAN SSH DARI SERVER INI BERARTI ANDA SETUJU SEGALA KETENTUAN YANG TELAH KAMI BUAT: " >> /etc/pesan
 echo "1. Tidak diperbolehkan untuk melakukan aktivitas illegal seperti DDoS, Hacking, Phising, Spam, dan Torrent di server ini; " >> /etc/pesan
 echo "2. Dilarang memberikan akun SSH / VPN / Proxy tanpa seizin admin, maks login $llimit kali; " >> /etc/pesan
-echo "2. Pengguna setuju jika kami mengetahui atau sistem mendeteksi pelanggaran di akunnya maka akun akan dihapus oleh sistem; " >> /etc/pesan
-echo "3. Tidak ada tolerasi bagi pengguna yang melakukan pelanggaran; " >> /etc/pesan
+echo "3. Pengguna setuju jika kami mengetahui atau sistem mendeteksi pelanggaran di akunnya maka akun akan dihapus oleh sistem; " >> /etc/pesan
+echo "4. Tidak ada tolerasi bagi pengguna yang melakukan pelanggaran; " >> /etc/pesan
 echo "Server by $namap ( $nhp )" >> /etc/pesan
 
 echo "Banner /etc/pesan" >> /etc/ssh/sshd_config
