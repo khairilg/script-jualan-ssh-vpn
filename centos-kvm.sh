879ad635d20830c8b209853973d643635922d98a
Khairil Gunawan <khairilgunawan@live.com>
2017-03-20 19:47:20 +0800

    Update centos-kvm.sh

--- centos-kvm.sh
+++ centos-kvm.sh
@@ -305,7 +305,7 @@ chkconfig crond on
 service crond stop
 echo "0 */12 * * * root sh /usr/bin/userexpire" > /etc/cron.d/user-expire
 echo "0 0 * * * root sh /usr/bin/reboot" > /etc/cron.d/reboot
-echo "* * * * * root userlimit $llimit" > /etc/cron.d/autokill
+echo "* * * * * root /bin/sh /usr/bin/cron-autokill" > /etc/cron.d/autokill
 #echo "0 */1 * * * root killall /bin/sh" > /etc/cron.d/killak
 
 # set time GMT +7
