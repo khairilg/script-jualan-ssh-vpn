07c62c2c84db4529de83d9c41256e57f10d5b414
Khairil Gunawan <khairilgunawan@live.com>
2017-03-20 19:03:16 +0800

    Create cron-autokill.sh

---
+++ cron-autokill.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Script untuk autokill cron by Khairil Gunawan
+
+mkdir -p "$HOME/tmp"
+PIDFILE="$HOME/tmp/myprogram.pid"
+
+if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
+                           grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
+  echo "Already running."
+  exit 99
+fi
+
+/usr/bin/autokill > $HOME/tmp/autokill.log &
+
+echo $! > "${PIDFILE}"
+chmod 644 "${PIDFILE}"
